f2ad63be8548ba7073160b3dcb7097062c8a1ced
Rakshit Diwani <rakshitdiwani@gmail.com>
2026-02-22 17:12:44 +0530

    feat: Add dashboard routes and controllers to display summary statistics and time-series graph data.

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -76,6 +76,102 @@ const getDashboardData = async (req, res) => {
     }
 }
 
+
+const getDashboardGraphData = async (req , res) =>{
+        const  range = req._parsedUrl.query;
+
+        if(![ "week" , "month" , "year"].includes(range)){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid range",
+                error: "Range should be week, month or year"
+            })
+        }
+
+    try {
+        const now = new Date();
+        let startDate = new Date();
+        let groupFormat;
+        let totalUnits;
+
+        if (range === "week") {
+            startDate.setDate(now.getDate() - 6);
+            startDate.setHours(0, 0, 0, 0);
+
+            groupFormat = { $dayOfWeek: "$createdAt" };
+            totalUnits = 7;
+        }
+
+        else if (range === "month") {
+            startDate.setDate(now.getDate() - 29);
+            startDate.setHours(0, 0, 0, 0);
+
+            groupFormat = { $dayOfMonth: "$createdAt" };
+            totalUnits = 30;
+        }
+
+        else if (range === "year") {
+            startDate.setMonth(now.getMonth() - 11);
+            startDate.setDate(1);
+            startDate.setHours(0, 0, 0, 0);
+
+            groupFormat = { $month: "$createdAt" };
+            totalUnits = 12;
+        }
+
+        const endDate = now;
+
+        const rawData = await invoiceModel.aggregate([
+            {
+                $match: {
+                    userId: new mongoose.Types.ObjectId(req.user.id),
+                    status: "paid",
+                    createdAt: { $gte: startDate, $lte: endDate }
+                }
+            },
+            {
+                $group: {
+                    _id: groupFormat,
+                    totalAmount: { $sum: "$totalAmount" }
+                }
+            }
+        ]);
+
+        const resultMap = {};
+
+        rawData.forEach(item => {
+            resultMap[item._id] = item.totalAmount;
+        });
+
+        const finalData = [];
+
+        for (let i = 1; i <= totalUnits; i++) {
+            finalData.push({
+                label: i,
+                totalAmount: resultMap[i] || 0
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Dashboard Graph Data Fetched Successfully",
+            range,
+            startDate,
+            endDate,
+            data: finalData
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error while fetching dashboard graph data",
+            error: error.message
+        })
+    }
+}
+
 export {
     getDashboardData,
+    getDashboardGraphData,
+    
 }
--- Backend/src/routes/dashboard.routes.js
+++ Backend/src/routes/dashboard.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getDashboardData } from "../controllers/dashboard.controller.js";
+import { getDashboardData, getDashboardGraphData } from "../controllers/dashboard.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get('/', authMiddleware, getDashboardData);
 
+router.get('/graph', authMiddleware, getDashboardGraphData);
 
 
 
