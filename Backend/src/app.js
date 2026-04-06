931177a5e0fe4b836eb9e74de843a20508fe2ac1
Rakshit Diwani <rakshitdiwani@gmail.com>
2026-04-06 17:55:12 +0530

    cors deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,8 +12,9 @@ const app = express();
 app.use(express.json({limit:"50mb"}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    // origin: "http://localhost:5173",
+    origin: "*",
+    // credentials: true
 }));
 app.use(express.urlencoded({limit:"50mb", extended: true }));
 app.use('/api/user' , userRoutes);
